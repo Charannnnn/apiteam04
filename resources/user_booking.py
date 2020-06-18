5454af2ca81cf5e0487141beefec2bf899c3b5b3
gsaiprathyusha <saiprathyushagidugu71847@gmail.com>
2020-06-18 20:52:39 +0530

    changed booking log

--- resources/user_booking.py
+++ resources/user_booking.py
@@ -11,16 +11,15 @@ class User_Bookings_log(Resource):
         parser=reqparse.RequestParser()
         parser.add_argument('id', type=str, required=True, help='user_id Cannot be blank')
         data= parser.parse_args()
-        result=[]
+        #result=[]
         try:
-            log1= query(f"""Select * from bookingHistory where user_id='{data["id"]}' and date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d")""",return_json=False)
-            log2= query(f"""Select * from bookingHistory where user_id='{data["id"]}' and date_format(day,"%Y-%m-%d")=(date_format(curdate()-1,"%Y-%m-%d"))""",return_json=False)
-            if(len(log1)!=0):
-                result.append(log1)
-            if(len(log2)!=0):
-                result.append(log2)
-            return jsonify(result)
-
+            return query(f"""Select * from bookingHistory where user_id='{data["id"]}' and date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d")""")
+            #log2= query(f"""Select * from bookingHistory where user_id='{data["id"]}' and date_format(day,"%Y-%m-%d")=(date_format(curdate()-1,"%Y-%m-%d"))""",return_json=False)
+            #if(len(log1)!=0):
+                #result.append(log1)
+            #if(len(log2)!=0):
+                #result.append(log2)
+            #return jsonify(result)
         except:
             return {"message": "There was an error connecting to bookings table"}, 500
 
