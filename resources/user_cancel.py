7de404258f89b12c1e150a82f985373d3d28498f
gsaiprathyusha <saiprathyushagidugu71847@gmail.com>
2020-06-20 21:06:18 +0530

    updated user delete

--- resources/user_cancel.py
+++ resources/user_cancel.py
@@ -8,26 +8,26 @@ from datetime import date,datetime,timedelta
 
 class cancelBooking(Resource):
     @jwt_required
-    def get(self):
+    def delete(self):
         parser=reqparse.RequestParser()
         parser.add_argument('id', type=str, required=True, help='user_id Cannot be blank')
         data= parser.parse_args()
-        result={}
+        #result={}
         try:
-            log1=query(f"""Select * from bookingHistory1 where user_id={data["id"]} and  date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d") and status<>1""",return_json=False)
+            log1=query(f"""Select * from bookingHistory1 where user_id='{data["id"]}' and  date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d") and status<>1""",return_json=False)
             
             if(len(log1)==0):
                 return {"message": "Can't Cancel your booking request"}, 500
             else:
-                query(f"""DELETE from booking where user_id={data["id"]} and  date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d") and status<>0""")
-                result["1"]=log1
-                result["message"]="Your booking has been canceled !!"
-                return jsonify(result)
+                query(f"""DELETE from booking where user_id='{data["id"]}' and  date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d") and status<>1""")
+                #result["1"]=log1
+                #result["message"]="Your booking has been canceled !!"
+                return {"message": "cancelled!"}, 200
         except:
             return {"message": "Cannot connect to the bookings table"}, 500
 
 class check(Resource):
-    def get(slef):
+    def get(self):
         try:
             now = datetime.now()
             now=now+timedelta(hours=5,minutes=30)
