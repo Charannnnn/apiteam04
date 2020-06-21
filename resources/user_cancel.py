27fc93a52ec6254f3ac531a317366613ae9d663d
gsaiprathyusha <saiprathyushagidugu71847@gmail.com>
2020-06-21 21:26:05 +0530

    updated user cancel to test

--- resources/user_cancel.py
+++ resources/user_cancel.py
@@ -12,16 +12,18 @@ class cancelBooking(Resource):
         parser=reqparse.RequestParser()
         parser.add_argument('id', type=str, required=True, help='user_id Cannot be blank')
         data= parser.parse_args()
-        #result={}
+        now = datetime.now()
+        now=now-timedelta(hours=4,minutes=30)
+        current_time = now.strftime("%H:%M:%S")
+        current_time=str(current_time)
+        data['current_time']=current_time
         try:
-            log1=query(f"""Select * from bookingHistory1 where user_id='{data["id"]}' and  date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d") and status<>1""",return_json=False)
+            log1=query(f"""Select * from bookingHistory1 where user_id='{data["id"]}' and time_to_sec(timediff('{data['current_time']}',reservation_time))/60 <=20 and date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d") and status<>1""",return_json=False)
             
             if(len(log1)==0):
-                return {"message": "Can't Cancel your booking request"}, 500
+                return {"message": "Can't Cancel your booking request"}, 400
             else:
-                query(f"""DELETE from booking where user_id='{data["id"]}' and  date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d") and status<>1""")
-                #result["1"]=log1
-                #result["message"]="Your booking has been canceled !!"
+                query(f"""UPDATE booking set status=CAST(2 AS UNSIGNED) where user_id='{data["id"]}' and  date_format(day,"%Y-%m-%d")=date_format(curdate(),"%Y-%m-%d") and status<>1""")
                 return {"message": "cancelled!"}, 200
         except:
             return {"message": "Cannot connect to the bookings table"}, 500
