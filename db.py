89b568057bad2e4680923ffd72114d48f2c271b9
gsaiprathyusha <saiprathyushagidugu71847@gmail.com>
2020-06-26 13:23:28 +0530

    updated timetable

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 import pymysql
 from flask import jsonify
+import base64
+
 def query(querystr, return_json=True):
     connection= pymysql.connect(
                                 host='skillup-team-04.cxgok3weok8n.ap-south-1.rds.amazonaws.com',
@@ -22,9 +24,14 @@ def query(querystr, return_json=True):
     else:
         return result
 
+def getBase64Str(value):
+    return base64.b64encode(value).decode('utf-8')
+
 def encode(data):
     for row in data:
         for key,value in row.items():
             if isinstance(value, Decimal):
                 row[key]=str(value)
+            elif isinstance(value, bytes):
+                row[key] = getBase64Str(value)
     return data
