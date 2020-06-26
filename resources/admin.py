df6e51999f5ac997b728fdb02d4848feb167eae5
gsaiprathyusha <57002577+gsaiprathyusha@users.noreply.github.com>
2020-06-26 23:16:54 +0530

    Update admin.py
    updated message to access_token

--- resources/admin.py
+++ resources/admin.py
@@ -28,7 +28,7 @@ class AdminLogin(Resource):
             access_token=create_access_token(identity=admin.id,expires_delta=False)
             return {'access_token':access_token}
         else:
-            return {'access_token':access_token}
+            return {"access_token":"Invalid credentials"},401
 
 class resource_(Resource):
     def __init__(self, id, name, count, resources_available):
